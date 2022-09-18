c1623d6b20ec978c8e859d70299576ee29c144de
Lucas <lucasthomazr@hotmail.com>
2022-09-18 16:35:07 -0300

    Started creating ads list page

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,39 +1,74 @@
-import { View, Image, FlatList } from 'react-native';
+import React, { useState } from 'react';
+import { Image, FlatList } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { useNavigation } from '@react-navigation/native';
+
 import logoImg from '../../assets/logo-nlw-esports.png';
-import { GameCard } from '../../components/GameCard';
+import { GameCard, GameCardProps } from '../../components/GameCard';
+import { Background } from '../../components/Background';
 import { Heading } from '../../components/Heading';
 
 import { GAMES } from '../../utils/games';
 
 import { styles } from './styles';
 
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  };
+}
+
 export function Home() {
+  const [games, setGames] = useState<Game[]>([]);
+
+  const navigation = useNavigation();
+
+  // useEffect(() => {
+  //   fetch('http://172.23.96.1:3333/games')
+  //     .then((response) => response.json())
+  //     .then((data) => setGames(data));
+
+  //   console.log(games)
+  // }, []);
+
+  function handleOpenGame({ id, title, bannerUrl }: GameCardProps) {
+    navigation.navigate('game', { id, title, bannerUrl });
+  }
+
   return (
-    <View style={styles.container}>
-      <Image
-        style={styles.logo}
-        source={logoImg}
-        defaultSource={logoImg}
-      />
+    <Background>
 
-      <Heading
-        title='Encontre seu duo!'
-        subtitle='Selecione o jogo que deseja jogar...'
-      />
+      <SafeAreaView style={styles.container}>
+        <Image
+          style={styles.logo}
+          source={logoImg}
+          defaultSource={logoImg}
+        />
 
-      <FlatList
-        data={GAMES}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => (
-          <GameCard
-            data={item}
-          />
-        )}
-        showsHorizontalScrollIndicator={false}
-        horizontal
-        contentContainerStyle={styles.contentList}
-      />
+        <Heading
+          title='Encontre seu duo!'
+          subtitle='Selecione o jogo que deseja jogar...'
+        />
 
-    </View>
+        <FlatList
+          data={GAMES}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <GameCard
+              data={item}
+              onPress={() => handleOpenGame(item)}
+            />
+          )}
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          contentContainerStyle={styles.contentList}
+        />
+
+      </SafeAreaView>
+    </Background>
   );
 }
--- src/utils/games.ts
+++ src/utils/games.ts
@@ -1,38 +1,194 @@
 export const GAMES = [
-  {
-    id: '1',
-    name: 'League of Legends',
-    ads: '4',
-    cover: require('../assets/games/game-1.png')
-  },
-  {
-    id: '2',
-    name: 'Dota 2',
-    ads: '3',
-    cover: require('../assets/games/game-2.png')
-  },
-  {
-    id: '3',
-    name: 'CS-GO',
-    ads: '2',
-    cover: require('../assets/games/game-3.png')
-  },
-  {
-    id: '4',
-    name: 'Apex',
-    ads: '3',
-    cover: require('../assets/games/game-4.png')
-  },
-  {
-    id: '5',
-    name: 'Fortnite',
-    ads: '5',
-    cover: require('../assets/games/game-5.png')
-  },
-  {
-    id: '6',
-    name: 'World of Warcraft',
-    ads: '2',
-    cover: require('../assets/games/game-6.png')
-  },
+	{
+		id: "4bb1e4ff-e58c-4620-b34b-a24468365454",
+		title: "League of Legends",
+		bannerUrl: "https://static-cdn.jtvnw.net/ttv-boxart/21779-188x250.jpg",
+		_count: {
+			ads: 1
+		}
+	},
+	{
+		id: "f022ed72-a66a-4355-811d-d142284eb303",
+		title: "Teamfight Tactics",
+		bannerUrl: "https://static-cdn.jtvnw.net/ttv-boxart/513143-188x250.jpg",
+		_count: {
+			ads: 4
+		}
+	},
+	{
+		id: "62c6577c-5d8e-43e2-829a-25597018fc23",
+		title: "Legends of Runeterra",
+		bannerUrl: "https://static-cdn.jtvnw.net/ttv-boxart/514790-188x250.jpg",
+		_count: {
+			ads: 1
+		}
+	},
+	{
+		id: "df2ba12b-fa68-4456-919a-c9f7d671c2e5",
+		title: "Elden Ring",
+		bannerUrl: "https://static-cdn.jtvnw.net/ttv-boxart/512953_IGDB-285x380.jpg",
+		_count: {
+			ads: 1
+		}
+	},
+	{
+		id: "8c52b122-5c3e-40ba-8a32-15db76379603",
+		title: "Fall Guys",
+		bannerUrl: "https://static-cdn.jtvnw.net/ttv-boxart/512980-285x380.jpg",
+		_count: {
+			ads: 0
+		}
+	},
+	{
+		id: "ea30a42b-e75f-4854-be79-d26342f3dff5",
+		title: "Tom Clancy's Rainbow Six Siege",
+		bannerUrl: "https://static-cdn.jtvnw.net/ttv-boxart/460630-285x380.jpg",
+		_count: {
+			ads: 0
+		}
+	},
+	{
+		id: "1219856f-a135-45dd-afdf-e0e2ceb7eb74",
+		title: "World of Warcraft",
+		bannerUrl: "https://static-cdn.jtvnw.net/ttv-boxart/18122-285x380.jpg",
+		_count: {
+			ads: 0
+		}
+	},
+	{
+		id: "0aefd1a1-2aef-4594-9ba5-e086376c3e54",
+		title: "Counter-Strike: Global Offensive",
+		bannerUrl: "https://static-cdn.jtvnw.net/ttv-boxart/32399_IGDB-285x380.jpg",
+		_count: {
+			ads: 0
+		}
+	},
+	{
+		id: "85efa70e-7566-4a3a-8a76-a51c38c65a26",
+		title: "Valorant",
+		bannerUrl: "https://static-cdn.jtvnw.net/ttv-boxart/516575-285x380.jpg",
+		_count: {
+			ads: 0
+		}
+	},
+	{
+		id: "4543ba66-8a47-45c0-8463-05d1f8ee18ec",
+		title: "Fortnite",
+		bannerUrl: "https://static-cdn.jtvnw.net/ttv-boxart/33214-285x380.jpg",
+		_count: {
+			ads: 0
+		}
+	},
+	{
+		id: "16e13127-fa9c-4d35-8a2b-b20642a4cba6",
+		title: "Lost Ark",
+		bannerUrl: "https://static-cdn.jtvnw.net/ttv-boxart/490100-285x380.jpg",
+		_count: {
+			ads: 0
+		}
+	},
+	{
+		id: "9ad5c022-7e67-463a-ac24-9f893ab61a49",
+		title: "V Rising",
+		bannerUrl: "https://static-cdn.jtvnw.net/ttv-boxart/766548668_IGDB-285x380.jpg",
+		_count: {
+			ads: 0
+		}
+	},
+	{
+		id: "6f19d98e-c9ab-4aa9-a1b9-a58db22e80a0",
+		title: "Free Fire",
+		bannerUrl: "https://static-cdn.jtvnw.net/ttv-boxart/502732_IGDB-285x380.jpg",
+		_count: {
+			ads: 0
+		}
+	},
+	{
+		id: "be600dde-87eb-4235-9dbd-42894cd7b2f5",
+		title: "GTA V",
+		bannerUrl: "https://static-cdn.jtvnw.net/ttv-boxart/32982_IGDB-285x380.jpg",
+		_count: {
+			ads: 0
+		}
+	},
+	{
+		id: "79cff918-cc30-4f6a-9ef0-f1251eba8492",
+		title: "Apex Legends",
+		bannerUrl: "https://static-cdn.jtvnw.net/ttv-boxart/511224-285x380.jpg",
+		_count: {
+			ads: 0
+		}
+	},
+	{
+		id: "e0850939-ce9f-473e-b92c-90bad429cfe1",
+		title: "Minecraft",
+		bannerUrl: "https://static-cdn.jtvnw.net/ttv-boxart/27471_IGDB-285x380.jpg",
+		_count: {
+			ads: 0
+		}
+	},
+	{
+		id: "fe3bae73-533f-4187-ab7e-8d7d4f558037",
+		title: "Dota 2",
+		bannerUrl: "https://static-cdn.jtvnw.net/ttv-boxart/29595-285x380.jpg",
+		_count: {
+			ads: 0
+		}
+	},
+	{
+		id: "d891fa2f-8672-4a58-9777-6e7ef6487c0b",
+		title: "Dead by Daylight",
+		bannerUrl: "https://static-cdn.jtvnw.net/ttv-boxart/491487-285x380.jpg",
+		_count: {
+			ads: 0
+		}
+	},
+	{
+		id: "a16244da-1fc0-4031-b4c0-e9a3de22bb08",
+		title: "Sea of Thieves",
+		bannerUrl: "https://static-cdn.jtvnw.net/ttv-boxart/490377-285x380.jpg",
+		_count: {
+			ads: 0
+		}
+	},
+	{
+		id: "71e58355-48f9-4176-b562-7724fd2cd507",
+		title: "Phasmophobia",
+		bannerUrl: "https://static-cdn.jtvnw.net/ttv-boxart/518184_IGDB-285x380.jpg",
+		_count: {
+			ads: 0
+		}
+	},
+	{
+		id: "64b9635a-069b-4440-a7d4-e3644351d5d7",
+		title: "PUBG: Battlegrounds",
+		bannerUrl: "https://static-cdn.jtvnw.net/ttv-boxart/493057-285x380.jpg",
+		_count: {
+			ads: 0
+		}
+	},
+	{
+		id: "74e20008-d1a4-4427-b6b1-a2d8227f4a94",
+		title: "Rocket League",
+		bannerUrl: "https://static-cdn.jtvnw.net/ttv-boxart/30921-285x380.jpg",
+		_count: {
+			ads: 0
+		}
+	},
+	{
+		id: "f590d69f-9edc-48ed-85e4-e67b3c6eba25",
+		title: "Magic: The Gathering",
+		bannerUrl: "https://static-cdn.jtvnw.net/ttv-boxart/2748-285x380.jpg",
+		_count: {
+			ads: 0
+		}
+	},
+	{
+		id: "a059d434-0636-43c1-8e6d-fd2987b5ee0d",
+		title: "Star Citizen",
+		bannerUrl: "https://static-cdn.jtvnw.net/ttv-boxart/71375_IGDB-285x380.jpg",
+		_count: {
+			ads: 0
+		}
+	}
 ];
